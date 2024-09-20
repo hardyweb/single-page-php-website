d8dc97c640c9e6a2b28351e668ac24681cfa2df7
Hardy N Jafar <nobat80@gmail.com>
2024-09-20 09:18:22 +0800

    Create index.php
    init

---
+++ index.php
@@ -0,0 +1,243 @@
+<?php
+
+$title = "Single Index.php Page";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $title ?></title>
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+>
+
+</head>
+<nav class="navbar has-background-black" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+
+    <a class="navbar-item" href="/">
+
+
+<svg
+   width="299.76875px"
+   height="84.8px"
+   viewBox="100.115625 32.6 299.76875 84.8"
+   style="background: rgba(0, 0, 0, 0);"
+   preserveAspectRatio="xMidYMid"
+   version="1.1"
+   id="svg3"
+   sodipodi:docname="hardyweb.svg"
+   inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <sodipodi:namedview
+     id="namedview3"
+     pagecolor="#ffffff"
+     bordercolor="#000000"
+     borderopacity="0.25"
+     inkscape:showpageshadow="2"
+     inkscape:pageopacity="0.0"
+     inkscape:pagecheckerboard="0"
+     inkscape:deskcolor="#d1d1d1"
+     showgrid="false"
+     inkscape:zoom="2.8321833"
+     inkscape:cx="149.88437"
+     inkscape:cy="14.299922"
+     inkscape:window-width="1366"
+     inkscape:window-height="697"
+     inkscape:window-x="-8"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="svg3" />
+  <defs
+     id="defs2">
+    <filter
+       id="editing-extrusion"
+       x="0"
+       y="0"
+       width="1.0123983"
+       height="1.0689465">
+      <feFlood
+         result="color1"
+         flood-color="#444"
+         id="feFlood1" />
+      <feConvolveMatrix
+         order="5,5"
+         divisor="1"
+         kernelMatrix="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1"
+         in="SourceAlpha"
+         result="extrude"
+         id="feConvolveMatrix1" />
+      <feComposite
+         in="color1"
+         in2="extrude"
+         result="comp-extrude"
+         operator="in"
+         id="feComposite1" />
+      <feOffset
+         dx="2.5"
+         dy="2.5"
+         in="comp-extrude"
+         result="offset-extrude"
+         id="feOffset1" />
+      <feMerge
+         id="feMerge2">
+        <feMergeNode
+           in="offset-extrude"
+           id="feMergeNode1" />
+        <feMergeNode
+           in="SourceGraphic"
+           id="feMergeNode2" />
+      </feMerge>
+    </filter>
+  </defs>
+  <g
+     filter="url(#editing-extrusion)"
+     id="g3"
+     transform="matrix(1.4341345,0,0,1.9291697,-109.959,-85.42787)">
+    <g
+       transform="translate(146.89999,93.130003)"
+       id="g2">
+      <path
+         d="M 23.92,-12.95 V 0 h -4.7 v -12.95 0 q 0,-0.93 -0.47,-1.4 v 0 0 q -0.47,-0.48 -1.41,-0.48 v 0 H 8.87 v -4.7 h 8.47 v 0 q 0.67,0 1.41,0.15 v 0 0 q 0.74,0.16 1.48,0.48 v 0 0 q 0.73,0.33 1.4,0.84 v 0 0 q 0.66,0.52 1.17,1.25 v 0 0 q 0.51,0.73 0.81,1.69 v 0 0 q 0.31,0.96 0.31,2.17 z M 6.97,-27.79 V 0 H 2.28 v -27.79 z m 41.41,14.73 v 6.59 0 q 0,0.98 -0.34,2.11 v 0 0 q -0.34,1.13 -1.11,2.09 v 0 0 q -0.77,0.97 -2.01,1.62 v 0 0 Q 43.68,0 41.91,0 v 0 h -8.47 v 0 q -0.97,0 -2.1,-0.34 v 0 0 q -1.13,-0.35 -2.1,-1.11 v 0 0 q -0.97,-0.77 -1.62,-2.01 v 0 0 Q 26.97,-4.7 26.97,-6.47 v 0 0 q 0,-0.97 0.35,-2.11 v 0 0 q 0.34,-1.14 1.11,-2.11 v 0 0 q 0.77,-0.96 2,-1.61 v 0 0 q 1.24,-0.65 3.01,-0.65 v 0 h 8.47 v 4.48 h -8.47 v 0 q -0.96,0 -1.48,0.58 v 0 0 q -0.52,0.59 -0.52,1.46 v 0 0 q 0,0.92 0.6,1.43 v 0 0 q 0.61,0.52 1.44,0.52 v 0 h 8.43 v 0 q 0.96,0 1.49,-0.58 v 0 0 q 0.52,-0.58 0.52,-1.44 v 0 -6.56 0 q 0,-0.92 -0.57,-1.47 v 0 0 q -0.57,-0.54 -1.44,-0.54 v 0 H 31.6 v -4.46 h 10.31 v 0 q 0.98,0 2.11,0.34 v 0 0 q 1.13,0.35 2.09,1.11 v 0 0 q 0.97,0.77 1.62,2.01 v 0 0 q 0.65,1.24 0.65,3.01 z m 21.1,-6.47 v 4.7 H 58.91 v 0 q -0.95,0 -1.44,0.48 v 0 0 q -0.49,0.47 -0.49,1.4 v 0 V 0 h -4.7 v -12.95 0 q 0,-1.21 0.31,-2.17 v 0 0 q 0.31,-0.96 0.82,-1.69 v 0 0 q 0.52,-0.73 1.19,-1.25 v 0 0 q 0.67,-0.51 1.4,-0.84 v 0 0 q 0.73,-0.32 1.48,-0.48 v 0 0 q 0.75,-0.15 1.4,-0.15 v 0 z m 24.45,-8.26 v 21.2 0 q 0,1.21 -0.31,2.17 v 0 0 q -0.31,0.97 -0.81,1.7 v 0 0 q -0.51,0.73 -1.18,1.25 v 0 0 q -0.67,0.51 -1.4,0.84 v 0 0 q -0.73,0.32 -1.48,0.48 v 0 0 Q 88,0 87.33,0 v 0 h -8.47 v 0 q -1.21,0 -2.17,-0.31 v 0 0 Q 75.73,-0.61 75,-1.13 v 0 0 q -0.73,-0.51 -1.24,-1.18 v 0 0 q -0.52,-0.67 -0.84,-1.4 v 0 0 q -0.33,-0.73 -0.48,-1.48 v 0 0 q -0.16,-0.75 -0.16,-1.4 v 0 -6.36 0 q 0,-1.81 0.66,-3.07 v 0 0 q 0.66,-1.25 1.65,-2.04 v 0 0 q 0.98,-0.78 2.14,-1.13 v 0 0 q 1.15,-0.34 2.13,-0.34 v 0 h 8.47 v 4.7 H 78.9 v 0 q -0.94,0 -1.43,0.48 v 0 0 q -0.49,0.47 -0.49,1.4 v 0 6.32 0 q 0,0.96 0.48,1.44 v 0 0 q 0.48,0.49 1.4,0.49 v 0 h 8.47 v 0 q 0.96,0 1.43,-0.49 v 0 0 q 0.47,-0.48 0.47,-1.4 v 0 -21.2 z m 26.01,8.26 v 21.41 0 q 0,1.21 -0.3,2.18 v 0 0 q -0.31,0.96 -0.82,1.69 v 0 0 q -0.5,0.74 -1.17,1.25 v 0 0 q -0.67,0.52 -1.4,0.84 v 0 0 q -0.73,0.33 -1.47,0.48 v 0 0 q -0.74,0.15 -1.41,0.15 v 0 h -8.48 V 3.78 h 8.48 v 0 q 0.94,0 1.41,-0.49 v 0 0 q 0.47,-0.49 0.47,-1.41 v 0 -21.41 z M 113.37,-4.7 V 0 h -8.48 v 0 q -1.01,0 -2.16,-0.34 v 0 0 q -1.14,-0.35 -2.14,-1.13 v 0 0 Q 99.6,-2.26 98.95,-3.51 v 0 0 Q 98.3,-4.77 98.3,-6.59 v 0 -12.94 h 4.7 v 12.94 0 q 0,0.92 0.48,1.4 v 0 0 q 0.49,0.49 1.45,0.49 v 0 z m 37.95,-14.83 h 5.26 v 0 q -1.12,2.51 -2.42,4.94 v 0 0 q -1.3,2.43 -2.72,4.6 v 0 0 q -1.42,2.17 -2.9,4 v 0 0 q -1.48,1.83 -2.93,3.16 v 0 0 q -1.46,1.33 -2.84,2.08 v 0 0 Q 141.39,0 140.16,0 v 0 0 q -0.51,0 -0.94,-0.18 v 0 0 q -0.43,-0.18 -0.76,-0.5 v 0 0 q -0.32,-0.31 -0.5,-0.75 v 0 0 q -0.19,-0.43 -0.19,-0.94 v 0 -8.76 l -9.32,10.43 v 0 q -0.36,0.36 -0.78,0.53 v 0 0 Q 127.24,0 126.79,0 v 0 0 q -0.49,0 -0.91,-0.18 v 0 0 q -0.43,-0.18 -0.75,-0.5 v 0 0 q -0.33,-0.31 -0.52,-0.75 v 0 0 q -0.19,-0.43 -0.19,-0.94 v 0 -17.16 h 4.7 V -8.4 l 9.36,-10.46 v 0 q 0.5,-0.51 1.2,-0.64 v 0 0 q 0.7,-0.14 1.36,0.15 v 0 0 q 0.67,0.27 1.06,0.86 v 0 0 q 0.39,0.59 0.39,1.29 v 0 11.17 0 q 1.52,-1.47 2.75,-3.09 v 0 0 q 1.22,-1.63 2.28,-3.35 v 0 0 q 1.06,-1.73 1.99,-3.51 v 0 0 q 0.93,-1.78 1.81,-3.55 z m 27.9,6.47 v 0 0 q 0,0.97 -0.34,2.1 v 0 0 q -0.35,1.13 -1.12,2.1 v 0 0 q -0.76,0.96 -2,1.62 v 0 0 q -1.24,0.65 -3.01,0.65 v 0 h -8.47 v -4.47 h 8.47 v 0 q 0.96,0 1.48,-0.58 v 0 0 q 0.53,-0.59 0.53,-1.46 v 0 0 q 0,-0.92 -0.59,-1.44 v 0 0 q -0.59,-0.53 -1.42,-0.53 v 0 h -8.47 v 0 q -0.96,0 -1.48,0.59 v 0 0 q -0.53,0.59 -0.53,1.45 v 0 6.56 0 q 0,0.94 0.59,1.47 v 0 0 q 0.59,0.52 1.45,0.52 v 0 h 8.44 V 0 h -8.47 v 0 q -0.98,0 -2.11,-0.34 v 0 0 q -1.13,-0.35 -2.09,-1.11 v 0 0 q -0.97,-0.77 -1.62,-2.01 v 0 0 q -0.65,-1.24 -0.65,-3.01 v 0 -6.59 0 q 0,-0.98 0.34,-2.11 v 0 0 q 0.35,-1.13 1.11,-2.09 v 0 0 q 0.77,-0.97 2.01,-1.62 v 0 0 q 1.24,-0.65 3.01,-0.65 v 0 h 8.47 v 0 q 0.98,0 2.11,0.34 v 0 0 q 1.12,0.35 2.09,1.11 v 0 0 q 0.97,0.77 1.62,2.01 v 0 0 q 0.65,1.24 0.65,3.01 z m 24.7,0.11 v 6.36 0 q 0,0.66 -0.16,1.4 v 0 0 q -0.15,0.75 -0.48,1.48 v 0 0 q -0.32,0.73 -0.84,1.4 v 0 0 q -0.51,0.67 -1.24,1.18 v 0 0 q -0.74,0.52 -1.69,0.82 v 0 0 Q 198.55,0 197.34,0 v 0 h -8.47 v 0 q -0.67,0 -1.41,-0.15 v 0 0 q -0.74,-0.16 -1.48,-0.48 v 0 0 q -0.73,-0.33 -1.4,-0.84 v 0 0 q -0.66,-0.52 -1.18,-1.25 v 0 0 q -0.51,-0.73 -0.82,-1.7 v 0 0 q -0.31,-0.96 -0.31,-2.17 v 0 -21.2 h 4.7 v 21.2 0 q 0,0.86 0.54,1.38 v 0 0 q 0.54,0.51 1.36,0.51 v 0 h 8.47 v 0 q 0.88,0 1.38,-0.52 v 0 0 q 0.5,-0.53 0.5,-1.37 v 0 -6.36 0 q 0,-0.89 -0.53,-1.39 v 0 0 q -0.52,-0.49 -1.35,-0.49 v 0 h -8.47 v -4.7 h 8.47 v 0 q 0.67,0 1.41,0.15 v 0 0 q 0.74,0.16 1.47,0.48 v 0 0 q 0.73,0.33 1.4,0.84 v 0 0 q 0.67,0.52 1.18,1.25 v 0 0 q 0.5,0.73 0.81,1.69 v 0 0 q 0.31,0.96 0.31,2.17 z"
+         fill="#cccccc"
+         id="path2" />
+    </g>
+  </g>
+  <style
+     id="style3">text {
+  font-size: 64px;
+  font-family: Arial Black;
+  dominant-baseline: central;
+  text-anchor: middle;
+}</style>
+</svg>
+
+    </a>
+
+   <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a> 
+</div>
+
+<div id="navbarBasicExample" class="navbar-menu">
+                <div class="navbar-end">
+                    <a class="navbar-item" href="/">Home</a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                       <!-- <a class="navbar-link">Gallery</a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">Gallery 1</a>
+                            <a class="navbar-item">Subpage</a>
+                            <a class="navbar-item">Subpage</a>
+                            <a class="navbar-item">Subpage</a> 
+                        </div> -->
+                    </div>
+        <a class="navbar-item" href="/about">About</a>
+                   
+ </div>
+                
+ </div>
+
+</nav>
+<body>
+
+<?php if($_SERVER['REQUEST_URI']=='/about'): ?>
+
+  <section class="section is-link is-fullheight">
+    <div class="container">
+      <h1 class="title">About</h1>
+      <h2 class="subtitle">
+       Pembinaan Laman ini menggunakan perisian sumber terbuka      </h2>
+
+     
+ 
+ 
+  <ul>
+  <li> Neovim ( Text Editor)</li> 
+
+  <li> Php </li> 
+  
+  <li> Bulma CSS</li> 
+  <li> Inkscape </li> 
+</ul>
+
+<hr/>
+ <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sit amet lacus eget malesuada. Vivamus fringilla elementum nisi, cursus condimentum risus semper vel. Fusce in nisi ac risus convallis varius. Donec pulvinar augue elementum nulla dictum, ut tincidunt neque eleifend. Nam tincidunt tristique justo in vehicula. Donec fermentum libero leo, fermentum ultrices elit vehicula ut. Duis id tempus erat, nec semper arcu. Integer finibus mauris ut augue tincidunt fermentum.
+ i,<br/>
+Ut vestibulum odio at rhoncus pharetra. Cras ac accumsan mi. Duis ut interdum diam, at iaculis mi. Cras accumsan elementum lacinia. Praesent commodo sem ac massa lacinia, non faucibus mauris elementum. Quisque finibus lacus a ex rhoncus consectetur. Duis ullamcorper dictum mauris sed dictum. Praesent accumsan ante velit, non molestie neque efficitur sit amet.
+,<br/>
+Nulla facilisi. Mauris sem massa, accumsan eu velit vitae, faucibus fermentum purus. Ut rhoncus elementum sem sit amet porta. Proin eu felis id ipsum pretium hendrerit. Ut in dui mollis, pellentesque eros et, aliquam lectus. Nunc elementum pretium tellus, id tempor odio posuere vel. Duis risus nisl, sodales id venenatis at, tristique ac lacus. Vivamus commodo mattis eleifend. Nullam lacus purus, feugiat eget urna in, iaculis commodo diam.
+      </p>
+    </div>
+  </section> 
+
+
+<?php else: ?>
+<section class="hero is-link is-fullheight">
+  <!-- Hero head: will stick at the top -->
+  <div class="hero-head">
+  </div>
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <p class="title">Open Source </p>
+      <p class="subtitle">Movement</p>
+    </div>
+  </div>
+ </div>
+
+</section>
+<?php endif; ?>
+
+<footer class="section has-background-black">
+  <div class="container">
+    <div class="pb-5 is-flex is-flex-wrap-wrap is-justify-content-between is-align-items-center">
+      <div class="mr-auto mb-2">
+        <a class="is-inline-block" href="#">
+       <h1> Ini Footer </h1> 
+        </a>
+      </div>
+      <div>
+        <ul class="is-flex is-flex-wrap-wrap is-align-items-center is-justify-content-center">
+          <li class="mr-4"><a class="button is-black" href="/about">About</a></li>
+          <!--<li class="mr-4"><a class="button is-black" href="#">Company</a></li>
+          <li class="mr-4"><a class="button is-black" href="#">Services</a></li>
+          <li><a class="button is-black" href="#">Lorem</a></li> -->
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="pt-5" style="border-top: 1px solid #dee2e6;"></div>
+  <div class="container">
+    <div class="is-flex-tablet is-justify-content-between is-align-items-center">
+      <p>Some Rights Reserved(Creative Commons) hardyweb.net 2024</p>
+      <div class="py-2 is-hidden-tablet"></div>
+      <div class="ml-auto">
+        <a class="mr-4 is-inline-block" href="#">
+        </a>
+        <a class="mr-4 is-inline-block" href="#">
+        </a>
+        <a class="mr-4 is-inline-block" href="#">
+        </a>
+        <a class="mr-4 is-inline-block" href="#">
+        </a>
+        <a class="is-inline-block" href="#">
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+
+</html>
+
